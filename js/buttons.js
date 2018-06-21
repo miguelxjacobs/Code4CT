4ecf6b7e30b46f5d57f5f40d67a0aa2e50c2d816
miguelxjacobs <miguel.jacobs@hotmail.com>
2018-06-21 13:54:10 +0200

    Fixed nav scroll effect

--- js/buttons.js
+++ js/buttons.js
@@ -4,21 +4,18 @@
 //Assigning navBar element to variable
 const nav = document.getElementById('nav_wrapper');
 //Assigning hamburger menu element to variable
+
+let open = true;
 const menu = document.getElementsByClassName('hamburger_icon')[0];
 //Displays the navBar
 menu.onclick = function() {
-    nav.style.display = "block";
+    if (open == true){
+        nav.style.marginTop = "0";
+        open = false;
+    }
+    else {
+        nav.style.marginTop = "-705px";
+        open = true;
+    }
+    
 }
-
-//Closes the navBar
-const close_menu = document.getElementsByClassName('donate_button')[0];
-close_menu.onclick = function() {
-    nav.style.display = "none";
-}
-
-//<Failsafe> Allows user to click outside of the modal to close Modal Window
-// window.onclick = function(event) {
-//     if (event.target == nav) {
-//         nav.style.display = "none";
-//     }
-// }
